1eb3bd36e03fee2d6b7e307293c3728c1cfc301e
Ivan-Leonardi <ivanthaisjesus@gmail.com>
2024-11-08 18:40:30 -0300

    feat: add navbar component

--- app/subscription/page.tsx
+++ app/subscription/page.tsx
@@ -1,5 +1,9 @@
+import Navbar from "../_components/navbar";
+
 const SubscriptionPage = () => {
-    return <h1>Subscription</h1>;
+    return (
+        <Navbar />
+    )
 };
 
 export default SubscriptionPage;
